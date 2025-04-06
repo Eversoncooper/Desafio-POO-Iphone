d89bdd2b21d06c6457922e3dd4040023afca7d29
Eversoncooper <everson.coopeer@gmail.com>
2025-04-05 21:25:48 -0300

    Desafio POO Iphone finalizado.

---
+++ Iphone.java
@@ -0,0 +1,29 @@
+public class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical{
+
+    public void ligar(String numero) {
+    }
+
+    public void atender() {
+    }
+
+    public void iniciarCorreioVoz() {
+    }
+
+    public void exibirPagina(String url) {
+    }
+
+    public void adicionarNovaAba() {
+    }
+
+    public void atualizarPagina() {
+    }
+
+    public void tocar() {
+    }
+
+    public void pausar() {
+    }
+
+    public void selecionarMusica(String musica) {
+    }
+}
